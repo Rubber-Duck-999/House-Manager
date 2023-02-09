de2dd6f63dba956421261f4fad7a2fe04ab0fc50
rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2023-02-09 21:37:55 +0000

    Update the parameter

--- src/main/java/com/rubber_duck/RestService/controller/TemperatureController.java
+++ src/main/java/com/rubber_duck/RestService/controller/TemperatureController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rubber_duck.RestService.model.Temperature;
@@ -28,7 +28,7 @@ public class TemperatureController {
     }
     
     @PostMapping("/temperature")
-    ResponseEntity<Temperature> createTemperature(@RequestBody Double temperature) {
+    ResponseEntity<Temperature> createTemperature(@RequestParam Double temperature) {
         return new ResponseEntity<>(temperatureService.createTemperature(temperature), HttpStatus.OK);
     }
 
