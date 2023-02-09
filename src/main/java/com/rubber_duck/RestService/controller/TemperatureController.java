2066c898ad156bbeeca983b0d676406fcaf5f952
rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2023-02-09 21:21:48 +0000

    Switch json body

--- src/main/java/com/rubber_duck/RestService/controller/TemperatureController.java
+++ src/main/java/com/rubber_duck/RestService/controller/TemperatureController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +29,8 @@ public class TemperatureController {
     }
     
     @PostMapping("/temperature")
-    ResponseEntity<Temperature> createTemperature(@RequestParam Double temp) {
-        return new ResponseEntity<>(temperatureService.createTemperature(temp), HttpStatus.OK);
+    ResponseEntity<Temperature> createTemperature(@RequestBody Double temperature) {
+        return new ResponseEntity<>(temperatureService.createTemperature(temperature), HttpStatus.OK);
     }
 
 }
