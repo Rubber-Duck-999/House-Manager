3bd739d7d3ff606379779574309b23ee231a18f8
rubber-duck-999 <simon.crowther95@hotmail.co.uk>
2023-04-30 17:20:41 +0100

    Fix missing imports on History.java

--- src/main/java/com/rubber_duck/RestService/model/History.java
+++ src/main/java/com/rubber_duck/RestService/model/History.java
@@ -1,5 +1,10 @@
 package com.rubber_duck.RestService.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
 @Getter
 @Setter
 @Document(collection="device-history")
